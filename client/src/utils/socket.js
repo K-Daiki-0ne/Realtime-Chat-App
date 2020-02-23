5a8df67e41b52730722ccb6a8c488533b4a6fbde
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-02-23 17:19:37 +0900

    Delete: comment

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,10 +1,5 @@
 import io from 'socket.io-client';
-// import { endPoint } from '../../Secret'
-
 const url = process.env.VUE_APP_END_POINT
 
-/* 
-  local環境で使う場合
-*/
 
 export const socket = io(url);
